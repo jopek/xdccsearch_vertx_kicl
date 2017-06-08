66527a5ffb4404aaeb0f2409a320e045e429dbed
Alexander Bluem <lxbluem@gmail.com>
2017-06-08 17:26:14 +0200

    sqlite connection!

--- src/main/java/com/lxbluem/filesystem/FileNameRepository.java
+++ src/main/java/com/lxbluem/filesystem/FileNameRepository.java
@@ -1,27 +1,74 @@
 package com.lxbluem.filesystem;
 
-import io.vertx.core.Vertx;
 import io.vertx.core.json.JsonObject;
-import io.vertx.ext.jdbc.JDBCClient;
-import io.vertx.ext.sql.SQLConnection;
+import io.vertx.rxjava.core.Vertx;
+import io.vertx.rxjava.ext.jdbc.JDBCClient;
+import io.vertx.rxjava.ext.sql.SQLConnection;
+
+import java.sql.*;
 
 public class FileNameRepository {
-    private final Vertx vertx;
-    private JDBCClient jdbcClient;
+    public static void main(String[] args) {
+        Connection connection = null;
+        try {
+            // create a database connection
+            connection = DriverManager.getConnection("jdbc:sqlite:sample.db");
+            Statement statement = connection.createStatement();
+            statement.setQueryTimeout(30);  // set timeout to 30 sec.
 
-    public FileNameRepository(Vertx vertx) {
-        this.vertx = vertx;
-        jdbcClient = JDBCClient.createNonShared(vertx, new JsonObject()
-                .put("url", "jdbc:sqlite:filenames.sqlite")
-//                .put("driver_class", "org.xerial.JDBC")
-        );
+            statement.executeUpdate("drop table if exists person");
+            statement.executeUpdate("create table person (id integer, name string)");
+            statement.executeUpdate("insert into person values(1, 'leo')");
+            statement.executeUpdate("insert into person values(2, 'yui')");
+            ResultSet rs = statement.executeQuery("select * from person");
+            while (rs.next()) {
+                // read the result set
+                System.out.println("name = " + rs.getString("name"));
+                System.out.println("id = " + rs.getInt("id"));
+            }
+        } catch (SQLException e) {
+            // if the error message is "out of memory",
+            // it probably means no database file is found
+            System.err.println(e.getMessage());
+        } finally {
+            try {
+                if (connection != null)
+                    connection.close();
+            } catch (SQLException e) {
+                // connection close failed.
+                System.err.println(e);
+            }
+        }
+    }
 
-        jdbcClient.getConnection(conn -> {
+    FileNameRepository(Vertx vertx) {
+
+        JsonObject config = new JsonObject()
+                .put("url", "jdbc:sqlite:sample.db")
+                .put("driver_class", "org.sqlite.JDBC")
+                .put("max_pool_size", 30);
+
+        JDBCClient client = JDBCClient.createShared(vertx, config, "MyDataSource");
+
+        client.getConnection(conn -> {
             if (conn.failed())
                 throw new RuntimeException(conn.cause());
 
-            SQLConnection connection = conn.result();
-            System.out.println("connection: " + connection);
+            client.getConnection(connectionAsyncResult -> {
+                if (connectionAsyncResult.succeeded()) {
+                    SQLConnection connection = connectionAsyncResult.result();
+
+                    connection.query("SELECT * FROM person", res2 -> {
+                        if (res2.succeeded()) {
+
+                            io.vertx.ext.sql.ResultSet rs = res2.result();
+                            System.out.println(rs.getRows());
+                        }
+                    });
+                } else {
+                    connectionAsyncResult.cause().printStackTrace();
+                }
+            });
         });
     }
 }
--- src/main/java/com/lxbluem/filesystem/FilenameResolverVerticle.java
+++ src/main/java/com/lxbluem/filesystem/FilenameResolverVerticle.java
@@ -1,20 +1,23 @@
 package com.lxbluem.filesystem;
 
-import io.vertx.core.AbstractVerticle;
-import io.vertx.core.json.JsonObject;
+import io.vertx.core.Launcher;
+import io.vertx.rxjava.core.AbstractVerticle;
 
 public class FilenameResolverVerticle extends AbstractVerticle {
-    private final FileNameRepository repository;
+    private FileNameRepository repository;
 
-    public FilenameResolverVerticle(FileNameRepository repository) {
-        this.repository = repository;
+
+    public static void main(String[] args) {
+        Launcher.main(new String[]{"run", FilenameResolverVerticle.class.getName()});
     }
 
     @Override
     public void start() {
-        vertx.eventBus().consumer("filename.resolve", handler -> {
-            JsonObject body = (JsonObject) handler.body();
-            String requestedFilename = body.getString("filename");
-        });
+//        vertx.eventBus().consumer("filename.resolve", handler -> {
+//            JsonObject body = (JsonObject) handler.body();
+//            String requestedFilename = body.getString("filename");
+//        });
+        repository = new FileNameRepository(vertx);
+
     }
 }

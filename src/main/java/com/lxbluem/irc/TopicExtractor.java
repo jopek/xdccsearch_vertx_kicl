f832e9a239da148f91279d5f74a09810bd76e77a
Alexander Bluem <lxbluem@gmail.com>
2017-06-26 12:40:31 +0200

    add topic extractor

---
+++ src/main/java/com/lxbluem/irc/TopicExtractor.java
@@ -0,0 +1,34 @@
+package com.lxbluem.irc;
+
+import java.util.HashSet;
+import java.util.Set;
+
+class TopicExtractor {
+
+    Set<String> getMentionedChannels(String topic) {
+        Set<String> channels = new HashSet<>();
+
+        for (String rawTokens : topic.toLowerCase().split("[\\[\\], ]")) {
+            String token = rawTokens.replaceAll("[^a-zA-Z#_-]", "");
+
+            if (token.isEmpty()) {
+                continue;
+            }
+
+            if (!token.startsWith("#")) {
+                continue;
+            }
+
+            if (token.contains("help")) {
+                continue;
+            }
+
+            if (!channels.contains(token)) {
+                channels.add(token);
+            }
+        }
+
+        return channels;
+    }
+
+}

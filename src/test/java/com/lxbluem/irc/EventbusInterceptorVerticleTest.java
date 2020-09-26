ed769be09b1e1df13bc44e1af7ab3aa1ee823c34
Alexander Bluem <lxbluem@gmail.com>
2020-09-27 01:11:54 +0200

    fix EventbusInterceptorVerticleTest by not using Junit Rules

--- src/test/java/com/lxbluem/irc/EventbusInterceptorVerticleTest.java
+++ src/test/java/com/lxbluem/irc/EventbusInterceptorVerticleTest.java
@@ -1,33 +1,26 @@
 package com.lxbluem.irc;
 
-import io.vertx.core.AbstractVerticle;
-import io.vertx.core.Future;
-import io.vertx.core.Handler;
-import io.vertx.core.Vertx;
+import io.vertx.core.*;
 import io.vertx.core.eventbus.DeliveryContext;
 import io.vertx.core.eventbus.EventBus;
 import io.vertx.ext.unit.Async;
 import io.vertx.ext.unit.TestContext;
-import io.vertx.ext.unit.junit.RunTestOnContext;
 import io.vertx.ext.unit.junit.VertxUnitRunner;
-import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(VertxUnitRunner.class)
 public class EventbusInterceptorVerticleTest {
-    @Rule
-    public RunTestOnContext rule = new RunTestOnContext();
-
     @Test(timeout = 5000)
     public void name(TestContext context) {
-        Vertx vertx = rule.vertx();
+        Vertx vertx = Vertx.vertx();
         vertx.eventBus().addOutboundInterceptor(interceptor("out"));
         vertx.eventBus().addInboundInterceptor(interceptor("in"));
 
-        TestVerticle testVerticle = new TestVerticle();
+        Verticle testVerticle = new TestVerticle();
         Async async = context.async();
         vertx.deployVerticle(testVerticle, context.asyncAssertSuccess(v -> async.complete()));
+        async.await();
         vertx.eventBus()
                 .request("topic", "TEST", context.asyncAssertSuccess(m -> System.out.println(m.body())));
 
@@ -45,7 +38,7 @@ public class EventbusInterceptorVerticleTest {
 
     private static class TestVerticle extends AbstractVerticle {
         @Override
-        public void start(Future<Void> startFuture) throws Exception {
+        public void start(Future<Void> startFuture) {
             EventBus eventBus = vertx.eventBus();
             eventBus.consumer("topic")
                     .handler(message -> {
@@ -53,7 +46,6 @@ public class EventbusInterceptorVerticleTest {
                         message.reply("REPLY LA");
                     })
                     .completionHandler(startFuture);
-//            startFuture.complete();
         }
     }
 }
